8306e82eca4b24ab26d080bb6c21eecae74015d6
Sumrit Gupta <32576036+awesumrit@users.noreply.github.com>
2018-05-23 10:54:58 +0530

    Update Titanic.m

--- Titanic Survivors/Titanic.m
+++ Titanic Survivors/Titanic.m
@@ -1,27 +1,25 @@
 
-clear ; close all; clc
+
 
 load('data.txt');
-X = data(:, [3,4,5,6,7]); y = data(:, 2);
+X = data(:, [3,4,5,6,7,8]); y = data(:, 2);
 
 [m, n] = size(X);
 
 X = [ones(m, 1) X];
 
-
-initial_theta = zeros(n + 1, 1);
-
-[cost, grad] = costFunction(initial_theta, X, y);
+test_theta = zeros(n+1,1);
+[cost, grad] = costFunction(test_theta, X, y );
 
 fprintf('Cost at initial theta (zeros): %f\n', cost);
 
 fprintf('Gradient at initial theta (zeros): \n');
-fprintf(' %f \n', grad);
+fprintf(' %f \n', grad(1:5));
 
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 
-[theta, cost] = ...
-	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+[theta, J, exit_flag] = ...
+	fminunc(@(t)(costFunction(t, X, y)), test_theta, options);
 
 fprintf('Cost at theta found by fminunc: %f\n', cost);
 fprintf('theta: \n');
@@ -30,28 +28,8 @@ m = size(X, 1);
 
 p = zeros(m, 1);
 
-index = find(sigmoid(X*theta)>=0.525) ;
+index = find(sigmoid(X*theta)>=0.5) ;
 p(index) =1; 
 
 fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
-
-
-
-
-% test(1,:)= [];
-% M=test ;
-% M(:,[2,3,4,5,6,7,8,9,10,11,12])= [];
-% test(:,[3,4,5,9,11,12])= [];
-% X = test(:, [2,3,4,5,6]);
-% [m, n] = size(X);
-
-% Add intercept term to x and X_test
-% X = [ones(m, 1) X];
-
-% m = size(X, 1); 
-
-% p = zeros(m, 1);
-
-% index = find(sigmoid(X*theta)>=0.75) ;
-% p(index) =1; 
 
